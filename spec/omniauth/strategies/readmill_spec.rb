763893937bf015e4558370d6c1217ab494cbd04d
Christof Dorner <christof@chdorner.com>
2012-11-04 15:21:37 +0100

    Clean up specs

--- spec/omniauth/strategies/readmill_spec.rb
+++ spec/omniauth/strategies/readmill_spec.rb
@@ -2,24 +2,12 @@ require 'spec_helper'
 
 describe OmniAuth::Strategies::Readmill do
   def app; lambda{|env| [200, {}, ["Hello."]]} end
-  let(:fresh_strategy){ Class.new(OmniAuth::Strategies::Readmill) }
+  let(:strategy) { Class.new(OmniAuth::Strategies::Readmill) }
 
   describe '#client' do
-    subject{ fresh_strategy }
+    subject { strategy.new(app) }
 
-    it 'configures the correct authorize url' do
-      instance = subject.new(app)
-      instance.client.options[:authorize_url].should == 'https://readmill.com/oauth/authorize'
-    end
-
-    it 'configures the correct token url' do
-      instance = subject.new(app)
-      instance.client.options[:token_url].should == 'https://readmill.com/oauth/token'
-    end
-
-    it 'adds scope non-expiring to authorize params' do
-      instance = subject.new(app)
-      instance.client.options[:authorize_params].should include(:scope => 'non-expiring')
-    end
+    specify { subject.client.options[:authorize_url].should == 'http://readmill.com/oauth/authorize' }
+    specify { subject.client.options[:token_url].should == 'http://readmill.com/oauth/token' }
   end
 end
